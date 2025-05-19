a522477a57cab503c8fa81d6630a3223ec8b09ac
Judith <j.espinalm12@gmail.com>
2025-05-18 20:14:24 -0400

    fix: task 4 and make it executable

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
@@ -10,43 +11,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *saved_head;
-    dlistint_t *tmp;
-    unsigned int p;
+	dlistint_t *current;
+	unsigned int i;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
-    saved_head = *head;
-    p = 0;
-    while (p < index && *head != NULL)
-    {
-        *head = (*head)->next;
-        p++;
-    }
-    if (p != index)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-    if (0 == index)
-    {
-        tmp = (*head)->next;
-        free(*head);
-        *head = tmp;
-        if (tmp != NULL)
-        {
-            tmp->prev = NULL;
-        }
-    }
-    else
-    {
-        (*head)->prev->next = (*head)->next;
-        if ((*head)->next)
-            (*head)->next->prev = (*head)->prev;
-        free(*head);
-        *head = saved_head;
-    }
-    return (1);
+	if (*head == NULL)
+		return (-1);
+
+	current = *head;
+	for (i = 0; current != NULL && i < index; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (-1);
+
+	if (current == *head)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
+
+	free(current);
+	return (1);
 }
