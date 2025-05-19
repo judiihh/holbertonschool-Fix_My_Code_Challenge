2cf09a5fbbd64f24a99ec823856bf9233e1b5ccd
Judith <j.espinalm12@gmail.com>
2025-05-18 20:15:54 -0400

    fix: delete file and make it executable

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,32 +11,42 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (*head == NULL)
 		return (-1);
 
-	current = *head;
-	for (i = 0; current != NULL && i < index; i++)
-		current = current->next;
-
-	if (current == NULL)
-		return (-1);
-
-	if (current == *head)
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
 	{
-		*head = current->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		*head = (*head)->next;
+		p++;
+	}
+
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+			tmp->prev = NULL;
 	}
 	else
 	{
-		current->prev->next = current->next;
-		if (current->next != NULL)
-			current->next->prev = current->prev;
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		free(*head);
+		*head = saved_head;
 	}
-
-	free(current);
 	return (1);
 }
